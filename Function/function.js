a4aa331aac3536e3a9dc744aff7f111e3e59ae39
bhagvanji nakum <bhagvanji@simformsolutions.com>
2023-03-16 11:14:18 +0530

    got idea about this keyword

--- Function/function.js
+++ Function/function.js
@@ -93,3 +93,17 @@ function foo(i) {
   console.log(`end: ${i}`);
 }
 foo(3);
+ 
+// this key word
+function getThis() {
+  return this;
+}
+
+const obj1 = { name: "obj1" };
+const obj2 = { name: "obj2" };
+
+obj1.getThis = getThis;
+obj2.getThis = getThis;
+
+console.log(obj1.getThis()); // { name: 'obj1', getThis: [Function: getThis] }
+console.log(obj2.getThis());
